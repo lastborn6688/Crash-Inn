d734eb5415339cb91a0cd0042ed7583a94b6c371
lastborn6688 <Ugochukwudanielmatthew@gmail.com>
2025-10-25 23:45:01 +0100

    commiting 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Hero from './components/Hero'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
+
   return (
-    <div>
-      
+    <div >
+      { !isOwnerPath && <Navbar/>}
+      <div className='min-h-[70vh]'>
+        <Hero/>
+      </div>
     </div>
   )
 }
